676d551e72c5ab5b0d5bff08808e254315ab1efa
Tom Shephard <tom.shephard@hotmail.co.uk>
2020-09-28 15:04:06 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,9 @@
 # code the #greeting method here!
+<<<<<<< HEAD
 def greeting (name)
   puts "Hi! I'm HAL, what's your name?"
   puts "Hello #{name}. It's nice to meet you."
+=======
+def greeting
+>>>>>>> ac975dfeeef9a435d7bad2a23efc9f4201819bfd
 end
